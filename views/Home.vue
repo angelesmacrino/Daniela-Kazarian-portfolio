feac80db603df4c36cb1e1c3e92d7cbb1dcf0fcf
Angeles Macrino <63437504+angelesmacrino@users.noreply.github.com>
2021-06-10 00:06:21 -0300

    Add files via upload

---
+++ views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div class="row">
+
+    <img class="img-fluid" v-bind:src="require('../assets/' + mobileImg)" v-if="mobileView" />
+    <img class="img-fluid" v-bind:src="require('../assets/' + desktopImg)" v-if="!mobileView" />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      mobileImg: "M02.jpg",
+      desktopImg: "M01.jpg",
+      mobileView: false,
+      showNav: false,
+    };
+  },
+
+  methods: {
+      handleView() {
+          this.mobileView = window.innerWidth <= 700;
+              
+      },
+
+      
+  },
+
+   created() {
+      this.handleView();
+      this.$emit('hi')
+      window.addEventListener('resize', this.handleView)
+  }
+};
+</script>
+
+<style scoped>
+  img {
+    padding-right: 0px;
+    padding-top: 8px;
+  }
+</style>
+
